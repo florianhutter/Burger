358c71479d9c930928073b197d878914380ed3db
Florian Hutter <florianhutter@Florians-MacBook-Pro.local>
2017-08-13 22:38:58 -0400

    hope it works this time

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Parm Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BigMac', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Burger', FALSE);
