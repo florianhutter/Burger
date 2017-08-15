74762433cdf0e128d6ad2a9325ce695e7ea46da1
Florian Hutter <florianhutter@lawn-128-61-127-208.lawn.gatech.edu>
2017-08-15 18:55:03 -0400

    praying

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	
+
+
+ connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "burgers_db"
+  database: "ellul984ttlbrwgt"
 });
-
+};
 
 connection.connect(function(err) {
   if (err) {
